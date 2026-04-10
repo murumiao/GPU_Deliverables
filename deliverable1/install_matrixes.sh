f7fd8d4062a9f7cb99e2521f8d21767260bbf267
user <5917673+bettozzo@users.noreply.github.com>
2026-04-10 11:46:21 +0200

    cpu implementation

---
+++ deliverable1/install_matrixes.sh
@@ -0,0 +1,22 @@
+
+mkdir -p ./data
+rm -rf ./data
+mkdir -p ./data
+urls=(
+    "https://suitesparse-collection-website.herokuapp.com/MM/SNAP/soc-sign-Slashdot081106.tar.gz"
+
+
+    #undirected graph
+    "https://suitesparse-collection-website.herokuapp.com/MM/Mycielski/mycielskian19.tar.gz"
+
+    #2d/3d problem | solo diagonale (ish)
+    "https://suitesparse-collection-website.herokuapp.com/MM/Janna/Queen_4147.tar.gz"
+)
+
+for url in "${urls[@]}"; do
+    tmpdir=$(mktemp -d)
+    curl -L --insecure "$url" | tar -xz -C "$tmpdir"
+
+    find "$tmpdir" -name "*.mtx" -exec mv {} ./data/ \;
+    rm -rf "$tmpdir"
+done
